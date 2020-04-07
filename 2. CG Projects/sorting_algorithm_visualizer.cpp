0025432c611c8e1dd4fcae821abea2f1606cfb2a
MaruthiTutorials <maruthitutorials@gmail.com>
2020-04-07 14:09:10 +0530

    Gknow Update

--- 2. CG Projects/sorting_algorithm_visualizer.cpp
+++ 2. CG Projects/sorting_algorithm_visualizer.cpp
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<GL\glut.h>
-#define SORT_NO 4	// Number of sorting algorithms
+#define SORT_NO 4	// Number of sorting algorithms.
 #define MAX 50		// Number of values in the array
 #define SPEED 700	// Speed of sorting, must be greater than MAX always
 int a[MAX];			// Array
